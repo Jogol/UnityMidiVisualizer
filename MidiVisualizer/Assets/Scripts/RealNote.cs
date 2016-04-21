d2b4d2d16c3164f0368e759fc4417637eef9f942
Jonathan Golan <Jonathanbgolan@gmail.com>
2016-04-21 03:24:33 +0200

    Publish

---
+++ MidiVisualizer/Assets/Scripts/RealNote.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RealNote {
+
+    public double RealTime;
+    //public int NoteNumber;
+    public double BPM;
+    public double OffsetFromPrevious;
+    public long OldAbsoluteTime;
+    public float Factor; //RealTime/(OldTime/1000)
+
+    public override string ToString()
+    {
+        string ret = "RealTime: " + RealTime + " BPM: " + BPM + " OffsetFromPrevious: " + OffsetFromPrevious + " OldAbsoluteTime: " + OldAbsoluteTime;
+        return ret;
+    }
+}
